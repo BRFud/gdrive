a709809d7cc82bf8fc34411d70404d9706daf1c8
BRFud <36411658+BRFud@users.noreply.github.com>
2021-12-01 01:50:56 +1100

    Update download.go
    attempt continue on recursive download error

--- drive/download.go
+++ drive/download.go
@@ -110,7 +110,9 @@ func (self *Drive) DownloadQuery(args DownloadQueryArgs) error {
 func (self *Drive) downloadRecursive(args DownloadArgs) error {
 	f, err := self.service.Files.Get(args.Id).Fields("id", "name", "size", "mimeType", "md5Checksum").Do()
 	if err != nil {
-		return fmt.Errorf("Failed to get file: %s", err)
+		//return fmt.Errorf("Failed to get file: %s", err)
+		fmt.Printf("Failed to get file: %s", err)
+		return nil
 	}
 
 	if isDir(f) {
