d0fd4848745166f475da4ae6c7f164b75ffbebe1
BRFud <36411658+BRFud@users.noreply.github.com>
2021-12-02 02:44:21 +1100

    Update handlers_drive.go

--- handlers_drive.go
+++ handlers_drive.go
@@ -9,9 +9,9 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/prasmussen/gdrive/auth"
-	"github.com/prasmussen/gdrive/cli"
-	"github.com/prasmussen/gdrive/drive"
+	"github.com/BRFud/gdrive/auth"
+	"github.com/BRFud/gdrive/cli"
+	"github.com/BRFud/gdrive/drive"
 )
 
 const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
